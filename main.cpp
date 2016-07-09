e87748dae914179293aaf0f5848f12bceaf67a2e
samirant15 <samirant15@gmail.com>
2016-07-09 16:23:27 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,14 +12,63 @@ typedef struct
 }vehiculo;
 
 
-int leer(vehiculo **car);
+int leerVehiculos(vehiculo **car);
+int agregarVehiculos(vehiculo **car, int tamAnterior);
 
 int main()
 {
-    vehiculo carBuffer;
-    vehiculo **car;
-    car = new vehiculo*[0];
+    vehiculo **vehiculos;
+    vehiculos = new vehiculo*[0];
+    int vehiculoCant = 0;
+    vehiculoCant = leerVehiculos(vehiculos);
+    vehiculoCant = agregarVehiculos(vehiculos, vehiculoCant);
 
+    printf("\n---------------------------------------------------\n");
+    for(int k=0; k<vehiculoCant+1; k++)
+    {
+        printf("\n*Carro #%d:\n", k+1);
+        printf("\nvelocidad: ");
+        cout << vehiculos[k]->velocidad;
+        printf("\ntipo: ");
+        cout << vehiculos[k]->tipo;
+    }
+    printf("\n---------------------------------------------------\n");
+}
+
+int leerVehiculos(vehiculo **car)
+{
+    vehiculo carBuffer;
+    ifstream archivo("file.dat");
+    if(!archivo.is_open())
+    {
+        cout << "No se puede abrir el archivo " << endl;
+        return 0;
+    }
+    archivo.seekg(0, ios::end);
+    if(archivo.tellg() == 0)
+    {
+        cout << "Vacio" << endl;
+        return -1;
+    }
+    int total;
+    total = (archivo.tellg()/sizeof(vehiculo))+1;
+    printf("TOTAL: %d\n\n", total);
+    archivo.seekg(0, ios::beg);
+    for(int i=0; i<total; i++)
+    {
+        car[i] = new vehiculo;
+        archivo.read((char*)&carBuffer.tipo, sizeof(carBuffer.tipo));
+        archivo.read((char*)&carBuffer.velocidad, sizeof(carBuffer.velocidad));
+        strncpy(car[i]->tipo, carBuffer.tipo, 50);
+        car[i]->velocidad = carBuffer.velocidad;
+        cout << endl << "#" << i+1 << " " << car[i]->tipo << ", " << car[i]->velocidad;
+    }
+    return total-1;
+}
+
+int agregarVehiculos(vehiculo **car, int tamAnterior)
+{
+    vehiculo carBuffer;
     fstream archivo("file.dat", ios::in|ios::out|ios::app);
     if(!archivo.is_open())
     {
@@ -27,7 +76,7 @@ int main()
         return -1;
     }
     int n = 0;
-    int i=leer(car)+1;
+    int i=tamAnterior+1;
     do
     {
         printf("\nCarro #%d", i+1);
@@ -48,43 +97,6 @@ int main()
         if(n)
             i++;
     }while(n);
-    printf("\n---------------------------------------------------\n");
-    for(int k=0; k<i+1; k++)
-    {
-        printf("\n*Carro #%d:\n", k+1);
-        printf("\nvelocidad: ");
-        cout << car[k]->velocidad;
-        printf("\ntipo: ");
-        cout << car[k]->tipo;
-    }
-    printf("\n---------------------------------------------------\n");
-}
 
-int leer(vehiculo **car)
-{
-    vehiculo carBuffer;
-    ifstream archivo("file.dat");
-    if(!archivo.is_open())
-    {
-        cout << "No se puede abrir el archivo " << endl;
-        return 0;
-    }
-    archivo.seekg(0, ios::end);
-    int total;
-    total = (archivo.tellg()/sizeof(vehiculo))+1;
-    if(total==0)
-        return -1;
-    printf("TOTAL: %d\n\n", total);
-    archivo.seekg(0, ios::beg);
-    for(int i=0; i<total; i++)
-    {
-        car[i] = new vehiculo;
-        archivo.read((char*)&carBuffer.tipo, sizeof(carBuffer.tipo));
-        archivo.read((char*)&carBuffer.velocidad, sizeof(carBuffer.velocidad));
-        strncpy(car[i]->tipo, carBuffer.tipo, 50);
-        car[i]->velocidad = carBuffer.velocidad;
-        cout << endl << "#" << i+1 << " " << car[i]->tipo << ", " << car[i]->velocidad;
-    }
-    return total-1;
+    return i;
 }
-
